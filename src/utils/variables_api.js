80b3ea9c8499d24e2a79ed5dd51893f1f113ab07
Joseluis Gutierrez <joseluis.c.jlgl@gmail.com>
2023-10-10 22:44:27 -0400

    Actualizando la variable

--- src/utils/variables_api.js
+++ src/utils/variables_api.js
@@ -1,12 +1,7 @@
 
-<<<<<<< HEAD
 export const API_HOST_LOCAL ="https://mi-refugio.herokuapp.com";
 //export const API_HOST_LOCAL ="http://localhost:8080";
 ///export const API_HOST_PRODUCCION = "https://mi-refugio.herokuapp.com";
 export const API_HOST_PRODUCCION = "https://mi-refugio.herokuapp.com";
-=======
-export const API_HOST_LOCAL ="http://localhost:8080";
-export const API_HOST_PRODUCCION = "http://localhost:8080";
 //export const API_HOST_PRODUCCION = "https://mi-refugio.herokuapp.com";
->>>>>>> 0c84bcb2c19f8544764223345dc45872c4205b6a
 export const Token = "token";
