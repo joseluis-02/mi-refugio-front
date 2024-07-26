77ab07f69533ef6347455f55e0632454ab754605
Joseluis Gutierrez <joseluis.c.jlgl@gmail.com>
2024-07-25 23:03:14 -0400

    El nacimiento de la app v1 completado

--- src/utils/variables_api.js
+++ src/utils/variables_api.js
@@ -1,7 +1,6 @@
-
-export const API_HOST_LOCAL ="http://localhost:8080";
-//export const API_HOST_LOCAL ="http://localhost:8080";
-///export const API_HOST_PRODUCCION = "https://mi-refugio.herokuapp.com";
+//export const API_HOST_LOCAL = "https://6f77hx18-8080.brs.devtunnels.ms";
 export const API_HOST_PRODUCCION = "http://localhost:8080";
+///export const API_HOST_PRODUCCION = "https://mi-refugio.herokuapp.com";
+//export const API_HOST_PRODUCCION = "https://6f77hx18-8080.brs.devtunnels.ms";
 //export const API_HOST_PRODUCCION = "https://mi-refugio.herokuapp.com";
 export const Token = "token";
