2803f798dee4abaae51ac34c8c631d4d2d82b3dc
Joseluis Gutierrez <joseluis.c.jlgl@gmail.com>
2024-08-14 14:46:52 -0400

    Proyecto final con la Heroku

--- src/utils/variables_api.js
+++ src/utils/variables_api.js
@@ -1,5 +1,5 @@
 //export const API_HOST_LOCAL = "https://6f77hx18-8080.brs.devtunnels.ms";
-export const API_HOST_PRODUCCION = "http://localhost:8080";
+export const API_HOST_PRODUCCION = "https://bowatgiw45.execute-api.us-east-1.amazonaws.com/dev";
 ///export const API_HOST_PRODUCCION = "https://mi-refugio.herokuapp.com";
 //export const API_HOST_PRODUCCION = "https://6f77hx18-8080.brs.devtunnels.ms";
 //export const API_HOST_PRODUCCION = "https://mi-refugio.herokuapp.com";
